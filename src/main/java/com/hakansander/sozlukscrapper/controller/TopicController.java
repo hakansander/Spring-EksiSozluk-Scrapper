229395cae0a917c66342cbaf99e5ddda854f9a31
Hakan Sander <hakansander@gmail.com>
2020-12-30 13:39:54 +0300

    Add crossOrigin for React access

--- src/main/java/com/hakansander/sozlukscrapper/controller/TopicController.java
+++ src/main/java/com/hakansander/sozlukscrapper/controller/TopicController.java
@@ -3,6 +3,7 @@ package com.hakansander.sozlukscrapper.controller;
 import com.hakansander.sozlukscrapper.model.response.TopicResponse;
 import com.hakansander.sozlukscrapper.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class TopicController {
     @Autowired
     TopicService topicService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(value = "/topics")
     public TopicResponse topicList() {
         return topicService.topicListHandler();
