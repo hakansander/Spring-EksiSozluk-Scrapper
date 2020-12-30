a32fcff769d30f7a8aa560953cf6ec14cf87ee28
Hakan Sander <hakansander@gmail.com>
2020-12-30 20:40:48 +0300

    Add CORS config

--- src/main/java/com/hakansander/sozlukscrapper/SozlukscrapperApplication.java
+++ src/main/java/com/hakansander/sozlukscrapper/SozlukscrapperApplication.java
@@ -2,13 +2,7 @@ package com.hakansander.sozlukscrapper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
@@ -16,19 +10,6 @@ public class SozlukscrapperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SozlukscrapperApplication.class, args);
-	}
-
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("https://hs-scrapper.herokuapp.com"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
-				"Cache-Control", "Content-Type"));
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
 	}
 
 }
--- src/main/java/com/hakansander/sozlukscrapper/controller/TopicController.java
+++ src/main/java/com/hakansander/sozlukscrapper/controller/TopicController.java
@@ -3,7 +3,6 @@ package com.hakansander.sozlukscrapper.controller;
 import com.hakansander.sozlukscrapper.model.response.TopicResponse;
 import com.hakansander.sozlukscrapper.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,6 @@ public class TopicController {
     @Autowired
     TopicService topicService;
 
-    @CrossOrigin(origins = "https://hs-scrapper.herokuapp.com/")
     @GetMapping(value = "/topics")
     public TopicResponse topicList() {
         return topicService.topicListHandler();
